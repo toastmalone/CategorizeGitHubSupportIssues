52041418571b5edd5ecc60dafd55bf3dc21a0474
toastmalone <josh.m.gauntt@gmail.com>
2019-07-31 13:00:32 -0700

    Initial commit

---
+++ CategorizeGitHubSupportIssues/GitHubIssueData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace CategorizeGitHubSupportIssues
+{
+    public class GitHubIssue
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+
+        [LoadColumn(1)]
+        public string Area { get; set; }
+
+        [LoadColumn(2)]
+        public string Title { get; set; }
+        [LoadColumn(3)]
+        public string Description { get; set; }
+
+    }
+
+    public class IssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area; 
+    }
+
+}
---
+++ CategorizeGitHubSupportIssues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+
+namespace CategorizeGitHubSupportIssues
+{
+
+    class Program
+    {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+        static void Main(string[] args)
+        {
+            _mlContext = new MLContext(seed: 0);
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_trainDataPath, hasHeader: true);
+            var pipeline = ProcessData();
+        }
+
+        public static IEstimator<ITransformer> ProcessData()
+        {
+
+        }
+    }
+}
